0ad95979d8be01935dbd3b959da0ab4004a1a79f
binbashz <5732@holbertonstudents.com>
2023-07-07 07:18:59 -0700

    base_model calss

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+BaseModel module for AirBnB clone project.
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    Base class for all models.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel instance.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    )
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__,
+            self.id,
+            self.__dict__
+        )
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
