3cec776394983005e74b85c18c5e1f4b551649d7
binbashz <5732@holbertonstudents.com>
2023-07-07 07:25:14 -0700

    state

---
+++ models/state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+State, City, Place modules for AirBnB clone project.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class inherits from BaseModel.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new State instance.
+        """
+        super().__init__(*args, **kwargs)
