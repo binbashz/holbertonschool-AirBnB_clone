aa079b692c24f38c78305cee669a4b78d5f74f7f
gabrielblasina <5722@holbertonstudents.com>
2023-07-08 07:16:09 -0700

    amenity and review classes and file storage modification

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,12 +37,16 @@ class FileStorage:
         from models.place import Place
         from models.state import State
         from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
         class_dict = {
             "BaseModel": BaseModel,
             "User": User,
             "Place": Place,
             "State": State,
             "City": City,
+            "Amenity": Amenity,
+            "Review": Review
             }
 
         try:
